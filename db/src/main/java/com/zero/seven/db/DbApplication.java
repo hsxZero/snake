b089e36252f162a6e7e51e46d6d3e8bbca2473dc
hsx <1824911442@qq.com>
2021-05-20 13:39:17 +0800

    second commit

---
+++ db/src/main/java/com/zero/seven/db/DbApplication.java
@@ -0,0 +1,10 @@
+package com.zero.seven.db;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbApplication {
+    public static void main(String[] args) {
+
+    }
+}
